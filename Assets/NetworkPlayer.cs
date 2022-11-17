41b50a0d3f71e42ef73af2e628d4e7ee75c97bf5
Evan Bradley <evan-bradley@uiowa.edu>
2022-11-17 11:20:27 -0600

    left hand on network player working

--- Assets/NetworkPlayer.cs
+++ Assets/NetworkPlayer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using Mirror;
+using Unity.XR.CoreUtils;
 
 namespace QuickStart
 {
@@ -10,10 +11,16 @@ namespace QuickStart
     {
         public Transform leftHand;
         public Transform rightHand;
+        private Transform leftRig;
         public override void OnStartLocalPlayer()
         {
-            Camera.main.transform.SetParent(transform);
-            Camera.main.transform.localPosition = new Vector3(0, 0, 0);
+            //Camera.main.transform.SetParent(transform);
+            //Camera.main.transform.localPosition = new Vector3(0, 0, 0);
+            XROrigin rig = FindObjectOfType<XROrigin>();
+            Debug.Log(rig);
+            leftRig = rig.transform.Find("CameraOffset/LeftHand (Smooth locomotion)");
+            //Debug.Log("okay");
+            Debug.Log(leftRig);
         }
 
         // Update is called once per frame
@@ -26,19 +33,29 @@ namespace QuickStart
 
             // transform.Rotate(0, moveX, 0);
             // transform.Translate(0, 0, moveZ);
-            MapPosition(leftHand, XRNode.LeftHand);
-            MapPosition(rightHand, XRNode.RightHand);
+            MapPosition(leftHand, leftRig);
+            //MapPosition(rightHand, XRNode.RightHand);
             
         }
-
-        void MapPosition(Transform target, XRNode node)
+        void MapPosition(Transform target, Transform rigTransform)
         {
-            InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
+            //InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
             //InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Quaternion rotation);
 
-            target.position = position;
-            Debug.Log(position);
+            target.position = rigTransform.position;
+            target.rotation = rigTransform.rotation;
+            //Debug.Log(position);
             //target.rotation = rotation;
         }
+
+        //void MapPosition(Transform target, XRNode node)
+        //{
+            //InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
+            //InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Quaternion rotation);
+
+            //target.position = position;
+            //Debug.Log(position);
+            //target.rotation = rotation;
+        //}
     }
 }
